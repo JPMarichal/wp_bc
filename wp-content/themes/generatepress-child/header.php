98c26fda0c9fc1e3d2fb29aa3f6afbbf926f4ecb
Juan Pablo Marichal <juan.pablo.marichal@solera.com>
2026-07-08 14:00:42 -0600

    feat: add page-with-sidebar.php template and extract header setup to inc/header.php

--- wp-content/themes/generatepress-child/header.php
+++ wp-content/themes/generatepress-child/header.php
@@ -1,8 +1,3 @@
-<?php
-
-remove_action( 'generate_after_header', 'generate_add_navigation_after_header', 5 );
-remove_action( 'generate_after_header', 'generate_featured_page_header', 10 );
-?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
